f1ac30a7a14ad349adfb140c163f8bde4fb9f1cf
Brett Toomey <brettcwx@gmail.com>
2016-09-04 15:04:37 +0900

    Updated for 08-18 snapshot.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporMySQL",
     dependencies: [
-   		.Package(url: "https://github.com/vapor/mysql-driver.git", majorVersion: 0, minor: 4),
-   		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16),
+   		.Package(url: "https://github.com/BrettRToomey/mysql-driver.git", majorVersion: 0, minor: 0),
+   		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 17),
     ]
 )
